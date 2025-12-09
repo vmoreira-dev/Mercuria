a81783cce04ee3a084814a61c34f7b92e85f8305
Vinicius <93095246+vmoreira-dev@users.noreply.github.com>
2025-12-09 12:53:10 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,13 @@
-import "./globals.css";
-import { CartProvider } from "@/context/CartContext";
+import type { ReactNode } from "react";
 
-
-export const metadata = {
-  title: "Mercuria",
-  description: "Frosted UI",
-};
-
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <html lang="en">
-      <body
-  className="min-h-screen bg-cover bg-center"
-  style={{
-    backgroundImage: "url('/background.webp')",
-  }}
->
-
-  <CartProvider>
-    {children}
-  </CartProvider>
-</body>
-
+      <body>{children}</body>
     </html>
   );
 }
